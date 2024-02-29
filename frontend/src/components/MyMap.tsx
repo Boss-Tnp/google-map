76777e30feae1841a85eedf908b38267a4c0ec33
Tanapon Satusinprasert <tanapon.satu@gmail.com>
2024-03-01 00:02:35 +0700

    wip: display markers from mock location

--- frontend/src/components/MyMap.tsx
+++ frontend/src/components/MyMap.tsx
@@ -12,6 +12,21 @@ const center = {
   lng: 100.534954,
 }
 
+const locations = [
+  {
+    lat: 13.7422785,
+    lng: 100.532654,
+  },
+  {
+    lat: 13.7467385,
+    lng: 100.532654,
+  },
+  {
+    lat: 13.7454385,
+    lng: 100.512654,
+  },
+]
+
 function MyMap() {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
@@ -19,6 +34,7 @@ function MyMap() {
   })
 
   const [map, setMap] = useState<google.maps.Map | null>(null)
+  const [markers, setMarkers] = useState(locations)
 
   const onLoad = useCallback((map: google.maps.Map) => {
     const bounds = new window.google.maps.LatLngBounds(center)
@@ -31,14 +47,31 @@ function MyMap() {
     setMap(null)
   }, [])
 
+  const addMarker = (e: google.maps.MapMouseEvent) => {
+    if (e.latLng) {
+      setMarkers([
+        ...markers,
+        {
+          lat: e.latLng.lat(),
+          lng: e.latLng.lng(),
+        },
+      ])
+    }
+  }
+
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={center}
+      zoom={12}
       onLoad={onLoad}
       onUnmount={onUnmount}
-      onClick={() => console.log("clicked")}
-    ></GoogleMap>
+      onClick={addMarker}
+    >
+      {markers.map((marker) => (
+        <Marker position={{ lat: marker.lat, lng: marker.lng }} />
+      ))}
+    </GoogleMap>
   ) : null
 }
 
